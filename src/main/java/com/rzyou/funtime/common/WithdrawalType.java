f7ed280182cced99a012f693592102a281faa9d9
liaoliangping <liaoliangping@erayu.com>
2019-11-15 19:24:22 +0800

    1.7-1

---
+++ src/main/java/com/rzyou/funtime/common/WithdrawalType.java
@@ -0,0 +1,33 @@
+package com.rzyou.funtime.common;
+
+public enum WithdrawalType {
+
+    DESPOSIT_CARD(1,"银行卡领赏")
+    ,WX(2,"微信领赏")
+    ,ALIPAY(3,"支付宝领赏")
+
+    ;
+    private int value;
+    private String desc;
+
+    WithdrawalType(int value, String desc) {
+        this.value = value;
+        this.desc = desc;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
